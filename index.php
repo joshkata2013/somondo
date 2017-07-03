564fbcef62024a355f79a0d87b1111ece9f99f81
joshkata2013 <joshkata2013@gmail.com>
2017-07-03 03:49:09 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+#lgsd {
+	position:absolute;
+	width:90px;
+	height:80px;
+	left:5px;
+	z-index:1;
+}
+#hder {
+	position:absolute;
+	width:370px;
+	height:20px;
+	z-index:2;
+	left:100px;
+	background:#FFF;
+	font-family: Tahoma, Geneva, sans-serif;
+	text-transform:uppercase;
+	font-weight: bolder;
+	font-stretch:extra-expanded;
+	font-size:18px;
+	color: #006;
+	padding:10px 5px;
+	text-align:center;
+}
+#curv1 {
+	position:absolute;
+	width:500px;
+	height:150px;
+	z-index:3;
+	bottom:0px;
+	background:#004;
+	-webkit-border-bottom-left-radius:10px;
+	-moz-border-bottom-left-radius:10px;
+	-o-border-bottom-left-radius:10px;
+	-ms-border-bottom-left-radius:10px;
+	border-bottom-left-radius:10px;
+	-webkit-border-bottom-right-radius:10px;
+	-moz-border-bottom-right-radius:10px;
+	-o-border-bottom-right-radius:10px;
+	-ms-border-bottom-right-radius:10px;
+	border-bottom-right-radius:10px;
+
+}
+#curv2{
+	position:absolute;
+	width:500px;
+	height:130px;
+	z-index:4;
+	bottom:30px;
+	background:#FFF;
+	-webkit-border-bottom-left-radius:100px;
+	-moz-border-bottom-left-radius:100px;
+	-o-border-bottom-left-radius:100px;
+	-ms-border-bottom-left-radius:100px;
+	border-bottom-left-radius:150%300%;
+}
+#fltcurv{
+	position:absolute;
+	width:408px;
+	height:30px;
+	z-index:5;
+	bottom:30px;
+	left:92px;
+	background:#E00;
+	-webkit-border-bottom-left-radius:100px;
+	-moz-border-bottom-left-radius:50px;
+	-o-border-bottom-left-radius:50px;
+	-ms-border-bottom-left-radius:50px;
+	border-bottom-left-radius:140%350%;
+}
+#apDiv{
+	position:absolute;
+	width:500px;
+	height:40px;
+	z-index:3;
+	bottom:0px;
+	background:#E00;
+	-webkit-border-bottom-left-radius:10px;
+	-moz-border-bottom-left-radius:10px;
+	-o-border-bottom-left-radius:10px;
+	-ms-border-bottom-left-radius:10px;
+	border-bottom-left-radius:10px;
+	-webkit-border-bottom-right-radius:10px;
+	-moz-border-bottom-right-radius:10px;
+	-o-border-bottom-right-radius:10px;
+	-ms-border-bottom-right-radius:10px;
+	border-bottom-right-radius:10px;
+	}
+#data {
+	position:absolute;
+	width:280px;
+	height:135px;
+	left:95px;
+	top:55px;
+	z-index:6;
+}
+</style>
+</head>
+
+<body>
+<div class="container">
+
+  <div id="lgsd"><img src="img/logo.jpg" width="90" height="80"></div>
+ <div id="hder"> debit collection &amp; auctioneer .</div>
+  <div id="curv1"><a href="" class="motto">Providing solution to your business</a></div>
+  <div id="curv2"></div>
+  <div id="fltcurv"><a href="" class="mlnk">dssltd@gmail.com</a></div>
+  <div id="data">
+  <form name="logf" method="post" action="">
+  <div><label class="msg"></label></div>
+  <div><label class="txtfld">Username:</label></div>
+  <div><input type="text" class="txtel" name="usnm"></div>
+  <div><label class="txtfld">Password:</label></div> 
+  <div><input type="password" class="txtel" name="pcw"></div>
+  <div><input type="submit" name="subtn" value="Login"></div><br/>
+  <div class="lnk"><a href="">Forgot Password</a>&nbsp;&nbsp;&nbsp;<a href="company.php">Register</a></div>
+  </form>
+  
+  </div>
+</div>
+
+</body>
+</html>
